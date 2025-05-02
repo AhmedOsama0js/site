976f00322bd490f83001d49f3a16378d63492849
Ahmed Osama Awad Al-Saeed <135539823+AhmedOsama0js@users.noreply.github.com>
2025-05-03 00:26:08 +0300

    first3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "build/index.html"));
 });
 
 app.get("/wel", (req, res) => {
