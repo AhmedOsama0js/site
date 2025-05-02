747ff49ef890a774e5cc824acf883dc4e9274ec9
Ahmed Osama Awad Al-Saeed <135539823+AhmedOsama0js@users.noreply.github.com>
2025-05-03 00:13:07 +0300

    first2

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/wel", (req, res) => {
+  res.send("Hello World");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
